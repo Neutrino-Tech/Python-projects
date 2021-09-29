75250763ab882db5ea0196d4ed12b8e564c3d454
Anish Kumar <65021172+Neutrino-Tech@users.noreply.github.com>
2021-09-29 23:33:14 +0530

    Update percentage and grade calculator.py

--- percentage and grade calculator.py
+++ percentage and grade calculator.py
@@ -10,42 +10,24 @@ maths=int(input("Marks in Maths: "))
 science=int(input("Marks in Science: "))
 sst=int(input("Marks in Social Science: "))
 total_percentage = (english+hindi+maths+science+sst)/5
-print(name,'of class',c+'th',s,'scored',total_percentage,'%')
-if total_percentage>=97:
-	print("Your Grade: A+")
-elif total_percentage>=93:
-    print("Your Grade: A")
-elif total_percentage>=90:
-    print("Your Grade: A-")
-elif total_percentage>=87:
-    print("Your Grade: B+")
-elif total_percentage>=83:
-	print("Your Grade: B")
-elif total_percentage>=80:
-	print("Your Grade: B-")
-elif total_percentage>=77:
-    print("Your Grade: C+")
-elif total_percentage>=73:
-    print("Your Grade: C")
-elif total_percentage>=70:
-	print("Your Grade: C-")
-elif total_percentage>=67:
-	print("Your Grade: D+")
-elif total_percentage>=65:
-	print("Your Grade: D")
-elif total_percentage>=64:
-	print("Your Grade: D-")
-elif total_percentage>=60:
-	print("Your Grade: E")
-elif total_percentage>=50:
-	print("Your Grade: F")
-elif total_percentage>=40:
-	print("Your Grade: G")
+cgpa=int(total_percentage/9.5)
+print(name,'of class',c+'th',s,'scored',total_percentage,'% &',cgpa,"CGPA")
+if total_percentage>=91:
+	print("Your Grade: A1")
+elif total_percentage>=81:
+	print("Your Grade: A2")
+elif total_percentage>=71:
+	print("Your Grade: B1")
+elif total_percentage>=61:
+	print("Your Grade: B2")
+elif total_percentage>=51:
+	print("Your Grade: C1")
+elif total_percentage>=41:
+	print("Your Grade: C2")
 elif total_percentage>=33:
-    print("Your Grade: H")
+    print("Your Grade: D1")
 elif total_percentage<=32:
     print("FAIL")
 print("""\nCongratulation for your achievement
 Choose what you like
 Study hard in your upcoming classes""")
-
